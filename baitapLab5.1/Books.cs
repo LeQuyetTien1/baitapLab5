f32e90f3f50f8007f70e7be7a2e04e07bd2bf79c
MaiTheLongDraco <128013309+MaiTheLongDraco@users.noreply.github.com>
2023-10-31 18:33:18 +0700

    add

---
+++ baitapLab5.1/Books.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5bai5._1
+{
+    internal class Book
+    {
+        private int id;
+        private string title;
+        private int price;
+        private string publisher;
+
+        public int Id
+        {
+            get { return 1; }
+
+        }
+        public string Title
+        {
+            get { return "Test"; }
+
+        }
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public string Publisher
+        {
+            get { return publisher; }
+            set { publisher = value; }
+        }
+
+        #region
+        public void ToString()
+        {
+            Console.WriteLine("---Thong tin sach---");
+            Console.WriteLine("Ma id: " + Id);
+            Console.WriteLine("Tieu de: " + Title);
+            Console.WriteLine("Gia: " + Price);
+            Console.WriteLine("NXB: " + Publisher);
+        }
+        #endregion
+    }
+}
---
+++ baitapLab5.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Lab5bai5._1;
+using System;
+using System.Diagnostics;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Book book1 = new Book();
+        book1.Price = 100;
+        book1.Publisher = "Tien";
+
+
+        book1.ToString();
+    }
+}
